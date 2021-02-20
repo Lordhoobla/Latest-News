c5121b1652c11a1a80a9849856e602d97322a520
Lordhoobla <45267209+Lordhoobla@users.noreply.github.com>
2021-02-19 23:30:32 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,104 +1,96 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.lang.StringBuilder;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+import java.lang.Thread;
 public class Main{
 	@SuppressWarnings("deprecation")
 	public static void main(String[] args)throws Exception{
 		setProgressBar();
 		FileCreate.init();
+		Thread.sleep(2000);
 		progressBar.hide();
+		System.exit(1);
 	}
 	/*Creates the info.txt file in the same directory
 	   *CHANGE TO HTML WHEN PUTTING IN ECLIPSE
 	   */
 	public static void create(){
 	     try{
-	       File info=new File("info.txt");
+	       File info=new File("info.html");
 	       info.createNewFile();
-	     }catch(IOException e){System.err.println("no work create");}
+	     }catch(IOException e){debug.append("no work create\n");}
 	   }
 	   /**Use this to actually write to the info.txt file. 
 	    *NOTE: THIS METHOD CAN ONLY BE CALLED once
 	    */
 	public static void writeTo(String html){
 	     try{
-	       FileWriter fl=new FileWriter("info.txt");
+	       FileWriter fl=new FileWriter("info.html");
 	       StringBuilder x=new StringBuilder(html);
 	       //listUS.forEach(n->{x.append(links.get(n)+"\n");});
 	       fl.write(x.toString());
 	       fl.close();
-	     }catch(IOException e){System.err.println("no work writeTo");}
+	     }catch(IOException e){debug.append("no work writeTo\n");}
 	   }
 	//holds the progress bar for the file
 	private static JFrame progressBar=new JFrame();
+	//finds and holds the icon for the bar
+	private ImageIcon log=new javax.swing.ImageIcon(this.getClass().getResource("icon.png"));
 	//initializes the progress bar
 	protected static void setProgressBar(){
 		progressBar.setSize(400,200);
 		progressBar.setTitle("Loading");
 		progressBar.setLocationRelativeTo(null);
-		ImageIcon logo=new ImageIcon("icon.png");
+		ImageIcon logo=new Main().log;
 		progressBar.setIconImage(logo.getImage());
 		progressBar.setResizable(false);
 		progressBar.setBackground(Color.GRAY);
+		progressBar.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		progressBar.setVisible(true);
 	}
+	protected static int percent;
 	/*Updates the progress bar
 	 * @param per - the percentage/100 that it's at
 	 * @param load - what it's loading now
 	 */
-	protected static void progress(int per,String load,boolean loading){
+	protected static void progress(int per,String load,int loading){
+		percent=per;
 		Container c=progressBar.getContentPane();
 		c.setLayout(new BorderLayout());
 		Rect.x=Rect.y=0;Rect.w=400;Rect.l=200;
 		Rect.x2=Rect.y2=50;Rect.w2=275;Rect.l2=30;
-		Rect.x3=Rect.y3=55;Rect.w3=per*270/100;Rect.l3=20;
-		Rect.text="Loading: "+load;
+		Rect.x3=Rect.y3=55;Rect.w3=percent*265/100;Rect.l3=20;
+		Rect.text=load;
 		Rect.c=Color.DARK_GRAY;Rect.c2=Color.BLACK;Rect.c4=Color.WHITE;
-		if(loading){Rect.c3=Color.green;}
-		else{Rect.c3=Color.BLUE;}
+		switch(loading){
+			case 0:Rect.c3=Color.BLUE;break;
+			case 1:Rect.c3=Color.RED;break;
+			default:Rect.c3=Color.GREEN;
+		}
 		Rect r=new Rect();
 		c.add(r);
 		progressBar.setVisible(true);
 	}
-		//variables for translate
-	 private static final String CLIENT_ID="FREE_TRIAL_ACCOUNT",CLIENT_SECRET="PUBLIC_SECRET",ENDPOINT="http://api.whatsmate.net/v1/translation/translate";
-		/*
-		 * Translates the text into the language given
-		 * @param fromLang - the language that it's coming from
-		 * @param toLang - the language that it's converting to
-		 * @param text - the text being translated
-		 * @return the translated text
-		 * NOTE: en-english :: es-spanish :: iw-hebrew :: fr-french :: ru-russian :: de-german :: it-italian :: ko-korean :: ja-japanese :: gu-gujarati :: bn-bengali :: hi-hindi :: pt-portuguese
-		 */
-	public static String translate(String fromLang, String toLang, String text) throws Exception{
-		  String jsonPayload=new StringBuilder().append("{").append("\"fromLang\":\"").append(fromLang).append("\",").append("\"toLang\":\"").append(toLang).append("\",").append("\"text\":\"").append(text).append("\"").append("}").toString();
-	      URL url=new URL(ENDPOINT);
-		  HttpURLConnection conn=(HttpURLConnection)url.openConnection();
-		  conn.setDoOutput(true);
-		  conn.setRequestMethod("POST");
-		  conn.setRequestProperty("X-WM-CLIENT-ID",CLIENT_ID);
-		  conn.setRequestProperty("X-WM-CLIENT-SECRET",CLIENT_SECRET);
-		  conn.setRequestProperty("Content-Type","application/json");
-	      OutputStream os=conn.getOutputStream();
-		  os.write(jsonPayload.getBytes());
-		  os.flush();
-		  os.close();
-	      int statusCode=conn.getResponseCode();
-		  BufferedReader br=new BufferedReader(new InputStreamReader((statusCode==200)?conn.getInputStream():conn.getErrorStream()));
-		  String output,ret="";
-		  while((output=br.readLine())!=null){ret+=output;}conn.disconnect();
-		  if(ret.contains("error_message")){return "<";}
-	      return ret;
-		}
+	//contains debug info
+	protected static StringBuilder debug=new StringBuilder();
+	/**
+	 * @function :: prints the debug info while also making a new file "debug.txt" with the information
+	 */
+	protected static void printDebug(){
+		System.out.println("DEBUG INFO:\n\n"+debug.toString());
+		try{
+			File f=new File("debug.txt");
+			f.createNewFile();
+			FileWriter fl=new FileWriter("debug.txt");
+			fl.write(debug.toString());
+			fl.close();
+		}catch(Exception e){e.printStackTrace();}
+	}
 }
